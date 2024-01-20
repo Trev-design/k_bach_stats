83a8fad4fbbfa95e5cafe7cde03ef0bfc9de6229
Tev-design <gerrit.flick@gmail.com>
2024-01-20 14:34:25 +0100

    first design considerations

--- client/src/components/navbar/styles.jsx
+++ client/src/components/navbar/styles.jsx
@@ -11,18 +11,26 @@ export const NavbarContainer = styled.section`
     justify-content: right;
     align-items: center;
     z-index: 1;
-    background: white;
+    background-color: black;
 `
 
 export const NavbarButton = styled(NavLink)`
     font-size: 1.1rem;
     margin: 0 2rem 0 0;
+    text-decoration: none;
+    @media screen and (max-width: 1020px) {font-size: 1rem;}
+    color: rgb(195, 165, 110);
+    background-color: black;
     cursor: pointer;
 `
 
 export const NavbarNavButton = styled.a`
     font-size: 1.1rem;
     margin: 0 2rem 0 0;
+    @media screen and (max-width: 1020px) {font-size: 0.9rem;}
+    @media screen and (max-width: 860px) {margin: 0 1rem 0 0;}
+    color: rgb(165, 165, 150);
+    background-color: black;
     cursor: pointer;
 `
 export const LandingSectionNavContainer = styled.div`
@@ -34,6 +42,8 @@ export const LandingSectionNavContainer = styled.div`
     flex-direction: row;
     justify-content: right;
     align-items: center;
+    @media screen and (max-width: 860px) {margin: 1rem;}
+    background-color: black;
 `
 
 export const NavbarLogoContainer = styled.div`
@@ -46,6 +56,7 @@ export const NavbarLogoContainer = styled.div`
 `
 
 export const NavbarLogo = styled.h1`
-    font-size: 1.2rem;
+    font-size: 1.4rem;
     padding-left: 2rem;
+    color: rgb(165, 165, 150);
 `
