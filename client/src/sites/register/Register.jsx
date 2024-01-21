50d6720b515c528b416b4f355dfe26e870207b11
Tev-design <gerrit.flick@gmail.com>
2024-01-21 11:48:42 +0100

    prepared register form roughly for styling

--- client/src/sites/register/Register.jsx
+++ client/src/sites/register/Register.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import { RegisterPageContainer } from './styles'
+import { RegisterLabel, RegisterPageContainer, RegisterValidationIcon } from './styles'
 import { useRef, useState, useEffect } from 'react'
 import {faCheck, faTimes, faInfoCircle} from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeICON } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function Register() {
   const EMAIL_REGEX = /^[A-Za-z0-9._%+-§$%!?]+@[A-Za-z0-9.-]+\.[a-z]{2,4}$/
@@ -47,7 +47,89 @@ function Register() {
 
   return (
     <RegisterPageContainer>
-      Register
+      <section>
+      <p ref={errRef} aria-aria-live='assertlive'>{errorMessage}</p>
+      <h1> Register </h1>
+      <form action="">
+        <RegisterLabel htmlFor="email">
+          Email:
+          <RegisterValidationIcon validInput={validEmail && user != ''}>
+            <FontAwesomeIcon icon={faCheck}/>
+          </RegisterValidationIcon>
+          <RegisterValidationIcon validInput={!validEmail && user != ''}>
+            <FontAwesomeIcon icon={faTimes}/>
+          </RegisterValidationIcon>
+        </RegisterLabel>
+        <input 
+          type="text"
+          id='email'
+          ref={userRef}
+          autoComplete='off'
+          onChange={e => setUser(e.target.value)}
+          required
+          aria-invalid={validEmail ? "false" : "true"}
+          aria-describedby='uidnote'
+          onFocus={() => setUserfocus(true)}
+          onBlur={() => setUserfocus(false)}
+        />
+        <p id='uidnote'>
+          <FontAwesomeIcon icon={faInfoCircle}/>
+          invalid email
+        </p>
+
+        <RegisterLabel htmlFor="password">
+          Email:
+          <RegisterValidationIcon validInput={validPassword && password != ''}>
+            <FontAwesomeIcon icon={faCheck}/>
+          </RegisterValidationIcon>
+          <RegisterValidationIcon validInput={!validPassword && password != ''}>
+            <FontAwesomeIcon icon={faTimes}/>
+          </RegisterValidationIcon>
+        </RegisterLabel>
+        <input 
+          type="password"
+          id='password'
+          ref={userRef}
+          autoComplete='off'
+          onChange={e => setPassword(e.target.value)}
+          required
+          aria-invalid={validPassword ? "false" : "true"}
+          aria-describedby='pwdnote'
+          onFocus={() => setPasswordFocus(true)}
+          onBlur={() => setPasswordFocus(false)}
+        />
+        <p id='pwdnote'>
+          <FontAwesomeIcon icon={faInfoCircle}/>
+          password must have at least 10 characters and lowercase and uppercase letters. also it needs to have digits and special characters like %&$§!?#€&
+        </p>
+
+        <RegisterLabel htmlFor="confirm">
+          Email:
+          <RegisterValidationIcon validInput={validConfirmation && confirmation != ''}>
+            <FontAwesomeIcon icon={faCheck}/>
+          </RegisterValidationIcon>
+          <RegisterValidationIcon validInput={!validConfirmation && confirmation != ''}>
+            <FontAwesomeIcon icon={faTimes}/>
+          </RegisterValidationIcon>
+        </RegisterLabel>
+        <input 
+          type="password"
+          id='confirm'
+          ref={userRef}
+          autoComplete='off'
+          onChange={e => setConfirmation(e.target.value)}
+          required
+          aria-invalid={validConfirmation ? "false" : "true"}
+          aria-describedby='confirmnote'
+          onFocus={() => setConfirmationFocus(true)}
+          onBlur={() => setConfirmationFocus(false)}
+        />
+        <p id='confirmnote'>
+          <FontAwesomeIcon icon={faInfoCircle}/>
+          confirmation does not match
+        </p>
+      </form>
+      </section>
     </RegisterPageContainer>
   )
 }
--- client/src/sites/register/styles.jsx
+++ client/src/sites/register/styles.jsx
@@ -3,4 +3,18 @@ import { PageContainer } from "../../styles"
 
 export const RegisterPageContainer = styled(PageContainer)`
     display: flex;
+    flex-direction: column;
+    width: 100vw;
+    height: 100vh;
+    align-items: center;
+    justify-content: center;
+    border: 1px solid white;
+`
+
+export const RegisterLabel = styled.label`
+    display: block;
+`
+
+export const RegisterValidationIcon = styled.span`
+    visibility: ${props => props.validInput ? "visible" : "hidden"};
 `
