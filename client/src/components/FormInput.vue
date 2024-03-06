440de6e6afb2ec3dc627aaddaef05ed8b45d71e1
Tev-design <gerrit.flick@gmail.com>
2024-03-06 19:58:03 +0100

    make form structure

---
+++ client/src/components/FormInput.vue
@@ -0,0 +1,72 @@
+<template>
+  <section class="form-input-container">
+    <form class="input-form">
+      <label class="form-label" for="">Name:</label>
+      <input class="input-area" type="text" required v-model="userName">
+
+      <label class="form-label" for="">Email:</label>
+      <input class="input-area" type="email" required v-model="email">
+
+      <label class="form-label" for="">Password:</label>
+      <input class="input-area" type="password" required v-model="password">
+
+      <label class="form-label" for="">Confirmation;</label>
+      <input class="input-area" type="password" required v-model="confirmation">
+
+      <div class="submit">
+        <button class="submit-button">Create Account</button>
+      </div>
+    </form>
+  </section>
+</template>
+
+
+<script>
+export default {
+    name: 'FormInput',
+    data: () => (
+      {
+        userName: '',
+        email: '',
+        password: '',
+        confirmation: '',
+      }
+    ),
+}
+</script>
+
+
+<style>
+.form-input-container {
+  width: 400px;
+  height: 600px;
+  border: 1px solid black;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin: 70px 0 0 -200px;
+}
+
+.form-label {
+  margin: 2rem 0 .2rem 2rem;
+}
+
+.input-form {
+  display: flex;
+  flex-direction: column;
+}
+
+.input-area {
+  margin: 0 2rem;
+  height: 20px;
+}
+
+.submit {
+  margin: 3rem 0;
+  text-align: center;
+}
+
+.submit-button {
+  padding: .4rem 1rem;
+}
+</style>
--- client/src/pages/Register.vue
+++ client/src/pages/Register.vue
@@ -1,17 +1,21 @@
 <template>
-  <div>
-        
-  </div>
+  <section class="register-page-container">
+    <FormInput></FormInput>  
+  </section>
 </template>
 
 
 <script>
+import FormInput from '../components/FormInput.vue';
 export default {
-  name: 'Register'
+  name: 'Register',
+  components: {FormInput}
 }
 </script>
 
 
 <style>
-    
+    .register-page-container {
+      position: relative;
+    }
 </style>
