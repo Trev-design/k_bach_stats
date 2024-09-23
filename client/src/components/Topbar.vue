f44ae10540cce05b7cd30f07618d0121d3e6a46d
Trev-design <gerrit.flick@gmail.com>
2024-09-23 16:49:50 +0200

    layout structure for landing page

--- client/src/components/Topbar.vue
+++ client/src/components/Topbar.vue
@@ -1,13 +1,20 @@
 <template>
-  
+    <section class="topbar-container"></section>
 </template>
 
 <script>
 export default {
-
+  name: 'Topbar'
 }
 </script>
 
 <style scoped>
-
+.topbar-container {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  background: white;
+  height: 5rem;
+}
 </style>
--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,12 +1,33 @@
 <template>
+  <section class="homepage-con">
+    <Topbar/>
+    <Sidebar/>
+    <section class="homepage-content-container">
+
+    </section>
+  </section>
 </template>
 
 <script>
+import Sidebar from '../components/Sidebar.vue';
+import Topbar from '../components/Topbar.vue';
 export default {
-  name: 'HomePage'
+  name: 'HomePage',
+  components: {
+    Sidebar,
+    Topbar
+  }
 }
 </script>
 
 <style scoped>
 
+.homepage-content-container {
+  position: relative;
+  left: 2rem;
+  width: 85vw;
+  height: 100vh;
+  margin: 7rem;
+  border: 1px solid white;
+}
 </style>
