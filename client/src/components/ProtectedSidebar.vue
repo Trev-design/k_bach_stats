1af7f7c91bf9779ebd2c93ce3ba8b0b63d42440d
Trev-design <gerrit.flick@gmail.com>
2024-10-21 14:29:33 +0200

    setup for sidebar functionalities

---
+++ client/src/components/ProtectedSidebar.vue
@@ -0,0 +1,19 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+  name: 'ProtectedSidebar',
+  props: {
+    isUnfold: {
+      type: Boolean,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ client/src/components/ProtectedTopBar.vue
@@ -0,0 +1,20 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+  name: 'ProtectedTopBar',
+
+  props: {
+    isUnfold: {
+        type: Boolean,
+        Required: true
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
