2bb6ea5e15f9b5ebd7bcb22bc6090327f2866f13
Trev-design <gerrit.flick@gmail.com>
2025-12-07 23:01:50 +0100

    setup key retrieval

--- auth/cmd/api/utils/vaultclient/vaultclient.go
+++ auth/cmd/api/utils/vaultclient/vaultclient.go
@@ -1,1 +1,44 @@
 package vaultclient
+
+import (
+	"context"
+	"errors"
+
+	vault "github.com/hashicorp/vault/api"
+	auth "github.com/hashicorp/vault/api/auth/kubernetes"
+)
+
+func GetSecret() (string, error) {
+	config := vault.DefaultConfig()
+
+	client, err := vault.NewClient(config)
+	if err != nil {
+		return "", err
+	}
+
+	auth, err := auth.NewKubernetesAuth("auth")
+	if err != nil {
+		return "", err
+	}
+
+	info, err := client.Auth().Login(context.Background(), auth)
+	if err != nil {
+		return "", err
+	}
+
+	if info == nil {
+		return "", errors.New("no auth info was returned after login")
+	}
+
+	secret, err := client.KVv2("secret").Get(context.Background(), "master")
+	if err != nil {
+		return "", err
+	}
+
+	value, ok := secret.Data["key"].(string)
+	if !ok {
+		return "", errors.New("value type assertion failed")
+	}
+
+	return value, nil
+}
